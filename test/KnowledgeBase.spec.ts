7991e34a91633b18980b854fd0c6f6636ed60f59
Dan Pries <danielmpries@gmail.com>
2018-08-31 21:21:54 -0400

    cleaned up tests and added another predicate

--- test/KnowledgeBase.spec.ts
+++ test/KnowledgeBase.spec.ts
@@ -1,20 +1,29 @@
 import KnowledgeBase from '../src/KnowledgeBase'
 import Predicates from './fixtures/predicates'
-
 const data = require('../data/plants.json')
 
-describe('Dummy test', () => {
+describe('KnowledgeBase', () => {
   it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
 
-  it('DummyClass is instantiable', () => {
-    let obj = { class: 'gymnosperm', 'leaf shape': 'scalelike' }
-    let predicates = new Predicates()
+  it('KnowledgeBase is instantiable', () => {
     expect(new KnowledgeBase(data, null)).toBeInstanceOf(KnowledgeBase)
+  })
 
+  test('KnowledgeBase should leverage callbacks and concrete rules', () => {
+    let predicates = new Predicates()
+    let obj = { class: 'gymnosperm', 'leaf shape': 'scalelike' }
     let kb = new KnowledgeBase(data, predicates)
     let facts = kb.evaluate(obj)
     expect(facts.hasOwnProperty('isPlant')).toBeTruthy()
+  })
+
+  test('KnowledgeBase should leverage failed callbacks and concrete rules', () => {
+    let predicates = new Predicates()
+    let obj = { class: 'gymnosperm', 'leaf shape': 'scalelike' }
+    let kb = new KnowledgeBase(data, predicates)
+    let facts = kb.evaluate(obj)
+    expect(facts.hasOwnProperty('isHuman')).toBeFalsy()
   })
 })
--- test/fixtures/predicates.ts
+++ test/fixtures/predicates.ts
@@ -2,4 +2,8 @@ export default class Predicates {
   isPlant(facts: object): boolean {
     return facts.hasOwnProperty('leaf shape')
   }
+
+  isHuman(facts: object): boolean {
+    return facts.hasOwnProperty('social security number')
+  }
 }
